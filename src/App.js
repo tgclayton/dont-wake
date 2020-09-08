e9aed84c473c1f4d15302a7eba17fd08698e3a92
Tom Clayton <tgclayton1@gmail.com>
2020-09-09 11:21:07 +1200

    alarm icon shrinks on click

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Counter from './components/Counter'
 import AlarmImage from './alarm.jpg'
+import { alarmShrink } from './components/animations'
 
 export default function App() {
   const [targetNum, newTarget] = useState(Math.floor(Math.random() * 10) + 1)
   const [count, newCount] = useState(0)
   const [gameOver, setGameOver] = useState(null)
-
+ 
   // useEffect(() => {
   //   console.log('useffect occurred')
   //   checkTargetReached(targetNum, count)
   // })
 
-  function reset() {
-    getNewTargetNum()
-    setGameOver(null)
-  }
+  // function reset() {
+  //   getNewTargetNum()
+  //   setGameOver(null)
+  // }
 
   function getNewTargetNum() {
     const range = Math.floor(Math.random() * 3) + 1
@@ -31,6 +32,8 @@ export default function App() {
   }
 
   function incrementCount(count) {
+    const alarmIcon = document.getElementById('alarm-icon')
+    alarmIcon.animate(alarmShrink, 250)
     ++count
     newCount(count)
     checkTargetReached(targetNum, count)
@@ -54,7 +57,7 @@ export default function App() {
       {/* <button onClick={() => getNewTargetNum()}>Change Target</button> */}
       {/* <button onClick={() => incrementCount(count)}>Increment Count</button> */}
       <br/><br/>
-      <img className = 'alarm-image'
+      <img id = 'alarm-icon' className = 'alarm-image'
        src = {AlarmImage} 
        alt ='Alarm Clock Icon'
        onMouseDown={() => incrementCount(count)}
---
+++ src/components/animations.js
@@ -0,0 +1,11 @@
+export const alarmShrink = [
+
+  { // from
+    height: '300px',
+    width: '300px'
+  },
+  { // to
+    height: '250px',
+    width: '250px'
+  }
+]
