8b5362f070a21845acf227390019f33a7f7f84fc
Tom Clayton <tgclayton1@gmail.com>
2020-09-11 09:56:58 +1200

    remove warnings from production

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState }from 'react';
 import './App.css';
-import Counter from './components/Counter'
+// import Counter from './components/Counter'
 import AlarmImage from './alarm_clock.png'
 import { alarmShrink } from './components/animations'
 
